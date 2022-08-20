05adf969fd75d207c537078531360f390eb71db1
Code Monkey King <freesoft.for.people@gmail.com>
2022-08-20 10:19:50 +0300

    implemented x command, fixed curx navigation for empty lines

--- src/ved.c
+++ src/ved.c
@@ -191,7 +191,7 @@ void update_screen() {
 void move_cursor(int key) {
   line_t *row = (cury >= last_addr()) ? NULL : search_line_node(cury+1);
   switch(key) {
-    case 'h': if (curx != 0) curx--; break;
+    case 'h': if (curx >= 0) curx--; break;
     case 'l': if (row && curx < row->len-1) curx++; break;
     case 'k': if (cury != 0) cury--; break;
     case 'j': if (last_addr() && cury != last_addr()-1) cury++; break;
@@ -199,6 +199,7 @@ void move_cursor(int key) {
   row = (cury >= last_addr()) ? NULL : search_line_node(cury+1);
   int rowlen = row ? row->len : 0;
   if (curx > rowlen-1) curx = rowlen-1;
+  if (curx < 0) curx = 0;
 }
 
 /* insert new line */
@@ -328,6 +329,13 @@ void read_keyboard() {
       case 'i': vmode = 'I'; break;
       case 'a': vmode = 'I'; curx++; break;
       case 'A': vmode = 'I'; curx = search_line_node(cury+1)->len; break;
+      case 'x': {
+        int linelen = search_line_node(cury+1)->len;
+        if (curx >= 0 && curx < linelen) {
+          curx++; delete_char();
+          if (curx > linelen-2) curx=linelen-2;
+        }
+      } break;
     }
   } else if (vmode == 'I') {
     if (c == '\n') insert_new_line();
